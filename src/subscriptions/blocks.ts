19e890cde30dcb9e8472b2e11bd17f55a8ba6fa7
0xng <ng@defi.sucks>
2022-08-02 16:51:39 -0300

    feat: adding emitWhenCloseToBlock

--- src/subscriptions/blocks.ts
+++ src/subscriptions/blocks.ts
@@ -2,6 +2,7 @@ import { emitWhenCloseToCooldown } from './cooldown';
 import { Block } from '@ethersproject/abstract-provider';
 import { BigNumber, providers } from 'ethers';
 import { mergeMap, Observable, share, Subject } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 export function getNewBlocks(provider: providers.BaseProvider): Observable<Block> {
 	const blockSubject$ = new Subject<Block>();
@@ -15,6 +16,16 @@ export function getNewBlocks(provider: providers.BaseProvider): Observable<Block
 }
 
 export function emitWhenCloseToBlock(
+	provider: providers.BaseProvider,
+	targetBlock: number,
+	blocksBefore: number
+): Observable<Block> {
+	const block$ = getNewBlocks(provider);
+	const targetBlockBefore = targetBlock - blocksBefore;
+	return block$.pipe(filter((block) => block.number >= targetBlockBefore));
+}
+
+export function emitWhenCloseToWorkable(
 	provider: providers.BaseProvider,
 	lastWorkAt: BigNumber,
 	workCooldown: BigNumber,
