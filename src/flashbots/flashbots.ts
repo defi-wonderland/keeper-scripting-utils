c87367b953712d8b64486ed0ee811a04c09354df
gotzenx <78360669+gotzenx@users.noreply.github.com>
2022-09-07 16:39:07 -0300

    docs: blocks and flashbots  (#37)

--- src/flashbots/flashbots.ts
+++ src/flashbots/flashbots.ts
@@ -9,13 +9,45 @@ import {
 import chalk from 'chalk';
 import { providers } from 'ethers';
 
+/**
+ * Class in charge of simulating and sending bundles through the different private relayer providers.
+ *
+ * @dev
+ * This class is able to have multiple private relayer providers (Flashbots, Eden, ...) instantiated in it and
+ * use them to send bundles to every one of them.
+ * It is important to know that this class should only use the flashbots provider for simulations but will send the bundle
+ * through every private relayer provider.
+ */
 export class Flashbots {
+	/**
+	 * @param txSigner - 						 Instance of signer to sign the required transactions.
+	 * @param flashbotsProviders - 	 Array of different private relayer providers that will be used to send the same
+	 * 														 	 bundle to each provider. Very important notice: Flashbot provider should be in
+	 * 															 the first position of the array always. This is due to simulations.
+	 * @param shouldSimulateBundle - Flag to simulate bundles before being send.
+	 */
 	private constructor(
 		private txSigner: Signer,
 		private flashbotsProviders: FlashbotsBundleProvider[],
 		private shouldSimulateBundle: boolean
 	) {}
 
+	/**
+	 * Will initialize and return an instance of a Flashbots class.
+	 *
+	 * @dev
+	 * It will create an array of providers for every private relayer provider url passed in the parameters and use them
+	 * to create a new instance of Flashbots class.
+	 *
+	 * @param txSigner - 				 Instance of signer to sign the required transactions.
+	 * @param bundleSigner - 		 Instance of a bundle signer.
+	 * @param provider -         Network provider.
+	 * @param flashbotRelayers - Array of private relayer providers urls. Flashbot provider should always be first in the array.
+	 * @param simulateBundle -   Flag to simulate bundles before being send.
+	 * @param chainId - 				 Id of the network in use.
+	 *
+	 * @returns 	A new instance of the Flashbots class with all the relayers providers instantiated.
+	 */
 	static async init(
 		txSigner: Signer,
 		bundleSigner: Signer,
@@ -24,7 +56,6 @@ export class Flashbots {
 		simulateBundle: boolean,
 		chainId: number
 	): Promise<Flashbots> {
-		// create a provider for every relay defined in the config
 		const flashbotsProviders = await Promise.all(
 			flashbotRelayers.map((relay) => {
 				return FlashbotsBundleProvider.create(provider, bundleSigner, relay, chainId);
@@ -34,6 +65,19 @@ export class Flashbots {
 		return new Flashbots(txSigner, flashbotsProviders, simulateBundle);
 	}
 
+	/**
+	 * First step in the process of sending a bundle through flashbots. Will take the transactions, sign them and form the bundle
+	 * to be sent. Will also simulate the bundle if the option is provided in initialization. And finally will broadcast
+	 * the bundle.
+	 *
+	 * @param unsignedTxs - 	 Array of unsigned transactions that will form the bundle.
+	 * @param targetBlock - 	 The block in which the bundle should be included and mined.
+	 * @param staticDebugId -  Optional static id to help with debugging. Every bundle will share this id.
+	 * @param dynamicDebugId - Optional dynamic id to help with debugging. Every bundle will have a different dynamic id. This dynamic id will
+	 * 						  					 be recalculated every time a bundle is created.
+	 *
+	 * @returns 	A boolean that says if the bundle was included successfully or not.
+	 */
 	async send(
 		unsignedTxs: TransactionRequest[],
 		targetBlock: number,
@@ -58,6 +102,18 @@ export class Flashbots {
 		return false;
 	}
 
+	/**
+	 * Simulates the bundle to see if the transactions in it will go through without reverting.
+	 *
+	 * @dev
+	 * If simulations are enabled, the Flashbots provider should be first in the array of private relayers.
+	 *
+	 * @param provider - 	 	Network provider.
+	 * @param bundle - 	 		The bundle that should be simulated.
+	 * @param targetBlock - The block number where the bundle should be simulated.
+	 *
+	 * @returns 	A boolean that says if the bundle simulation passed without reverts.
+	 */
 	async simulateBundle(
 		provider: FlashbotsBundleProvider,
 		bundle: FlashbotsBundleRawTransaction[],
@@ -85,6 +141,22 @@ export class Flashbots {
 		return true;
 	}
 
+	/**
+	 * Function in charge of broadcasting the bundle through all the different private relayers providers.
+	 *
+	 * @dev
+	 * Will create an array of sentBundles (inclusions) promises and wait for all of them to be resolved. After that it will
+	 * check if any of the responses was true which means the bundle was included in one of the private relayers.
+	 *
+	 * @param provider - 	 		 Network provider.
+	 * @param bundle - 	 			 The bundle that should be simulated.
+	 * @param targetBlock - 	 The block number where the bundle should be included and mined.
+	 * @param staticDebugId -  Optional static id to help with debugging. Every bundle will share this id.
+	 * @param dynamicDebugId - Optional dynamic id to help with debugging. Every bundle will have a different dynamic id. This dynamic id will
+	 * 						  					 be recalculated every time a bundle is created.
+	 *
+	 * @returns 	A boolean that says if the bundle was included in any private relayer or not.
+	 */
 	async broadcastBundle(
 		providers: FlashbotsBundleProvider[],
 		bundle: FlashbotsBundleRawTransaction[],
@@ -101,6 +173,22 @@ export class Flashbots {
 		return inclusions.find((inclusion) => inclusion) || false;
 	}
 
+	/**
+	 * Function in charge of sending the bundle through the specified private relayer provider.
+	 *
+	 * @dev
+	 * Will send the bundle to the relayer provider and wait for its resolution. Depending on the type of resolution
+	 * it will return if the bundle was included or not.
+	 *
+	 * @param provider - 	 		 Network provider.
+	 * @param bundle - 	 			 The bundle that should be simulated.
+	 * @param targetBlock - 	 The block number where the bundle should be included and mined.
+	 * @param staticDebugId -  Optional static id to help with debugging. Every bundle will share this id.
+	 * @param dynamicDebugId - Optional dynamic id to help with debugging. Every bundle will have a different dynamic id. This dynamic id will
+	 * 						  					 be recalculated every time a bundle is created.
+	 *
+	 * @returns 	A boolean that says if the bundle was included in the private relayer or not.
+	 */
 	async sendBundle(
 		provider: FlashbotsBundleProvider,
 		bundle: FlashbotsBundleRawTransaction[],
--- src/subscriptions/blocks.ts
+++ src/subscriptions/blocks.ts
@@ -1,15 +1,43 @@
 import { Block } from '@ethersproject/abstract-provider';
 import chalk from 'chalk';
-import { BigNumber, providers } from 'ethers';
+import { providers } from 'ethers';
 import { from, fromEvent, merge, mergeMap, Observable, Subject, Subscription } from 'rxjs';
-import { filter, shareReplay } from 'rxjs/operators';
+import { shareReplay } from 'rxjs/operators';
 
+/**
+ * Class in charge of managing the fetching of blocks.
+ *
+ */
 export class BlockListener {
+	// Amount of live subscriptions to block$ observable.
 	private count = 0;
+
+	// Observable in charge of emiting and providing new Blocks.
 	private block$ = new Subject<Block>();
+
+	// Array of generated interal subscriptions. Used mainly to be able to unsubscribe from them when needed.
 	private subs: Subscription[] = [];
+
+	/**
+	 * @param provider - JsonRpc provider that has the methods needed to fetch and listen for new blocks.
+	 */
 	constructor(private provider: providers.BaseProvider) {}
 
+	/**
+	 * This function is able to provide a listener for new incoming blocks with all their data.
+	 * Returns and observable that emits an event every time a new block arrives.
+	 *
+	 * @dev
+	 * Block listener is initialized only if the subscriptions account is zero. Otherwise it will skip the initialization
+	 * and just return the block$ observable where the new blocks are being pushed.
+	 * For the fetching part we need to combine two different functions to fetch and deliver new blocks:
+	 *  - One that fetches the current block just once when this function is called and push it to block$ observable.
+	 *  - One that hooks to the 'block' event of the provider that returns just the number of the new block, and then use
+	 * 		provider.getBlock(blockNumber) method to fetch all the data of that block and push it to block$ observable.
+	 *
+	 * @param debugId - Optional id to help with debugging.
+	 * @returns An observable that emits blocks
+	 */
 	stream(debugId?: string): Observable<Block> {
 		if (this.count++ === 0) {
 			this.provider.getBlock('latest').then((block) => {
@@ -32,6 +60,16 @@ export class BlockListener {
 		return this.block$;
 	}
 
+	/**
+	 * Stops block fetching and remove all the interal subscriptions to blockNumber observable.
+	 *
+	 * @dev
+	 * This will only stop block fetching and subscription to blockNumber IF the amount of subscriptions to block$ observable
+	 * is zero. If amount is zero this means that theres no part of the code actually listening to the block$ observable
+	 * so theres no need for us to keep listening for new blocks incoming.
+	 *
+	 * @param debugId - Optional id to help with debugging.
+	 */
 	stop(debugId?: string): void {
 		if (--this.count === 0) {
 			console.info(chalk.redBright('\n------ STOP BLOCK LISTENING -----'));
@@ -46,27 +84,45 @@ export class BlockListener {
 	}
 }
 
-//TODO: Add detailed documentation
+/**
+ * This function is able to provide a listener for current and new incoming blocks with all their data.
+ *
+ * @dev
+ * It combines two different methods to fetch new blocks:
+ * - One that fetches the current block just once when this function is called.
+ * - One that starts listening for new blocks until stopped. When started this method will wait til next new block
+ * 	 to start emiting.
+ * Important to notice that this method will emit the current block when being called and also start listening for
+ * new incoming blocks.
+ *
+ * @param provider - JsonRpc provider that has the methods needed to fetch and listen for new blocks.
+ */
 export function getNewBlocks(provider: providers.BaseProvider): Observable<Block> {
 	console.log('start get new blocks');
 	return merge(from(provider.getBlock('latest')), blockListener(provider)).pipe(shareReplay(1));
 }
 
+/**
+ * This function is able to provide a listener for new incoming blocks with all their data.
+ *
+ * @dev
+ * It hooks to the 'block' event of the provider that returns just the number of the new block, to then use
+ * provider.getBlock(blockNumber) method to fetch all the data of the block.
+ * Important to notice that this method does not emit the current block when its called. First emition will be
+ * the next new block received after being called.
+ *
+ * @param provider - JsonRpc provider that has the methods needed to fetch and listen for new blocks.
+ */
 function blockListener(provider: providers.BaseProvider): Observable<Block> {
 	const onBlock$ = fromEvent(provider, 'block') as Observable<number>;
 	return onBlock$.pipe(mergeMap((block) => provider.getBlock(block)));
 }
 
-export function emitWhenCloseToBlock(
-	provider: providers.BaseProvider,
-	targetBlock: number,
-	blocksBefore: number
-): Observable<Block> {
-	const block$ = getNewBlocks(provider);
-	const targetBlockBefore = targetBlock - blocksBefore;
-	return block$.pipe(filter((block) => block.number >= targetBlockBefore));
-}
-
+/**
+ * Removes block listener on the provider.
+ *
+ * @param provider - JsonRpc provider to that has the methods needed to fetch and listen for new blocks.
+ */
 export function stopBlocks(provider: providers.BaseProvider): void {
 	console.log('------ STOP LISTENING -----');
 
